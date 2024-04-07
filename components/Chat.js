e448f41e781134d0ebf04c0dda66d8183d1e9c4b
Caduella <146942933+Caduella@users.noreply.github.com>
2024-04-07 21:30:11 +0200

    Add Gifted Chat features

--- components/Chat.js
+++ components/Chat.js
@@ -1,17 +1,68 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { StyleSheet, View, Text } from 'react-native';
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
     const { name, background } = route.params;
+    const [messages, setMessages] = useState([]);
+    const onSend = (newMessages) => {
+      setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+    }
+
+    //Customize speech bubble
+    const renderBubble = (props) => {
+      return <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#000"
+          },
+          left: {
+            backgroundColor: "#FFF"
+          }
+        }}
+      />
+    }
 
 		// Display the user's name
     useEffect(() => {
         navigation.setOptions({ title: name });
       }, []);
 
+    // Display two preloaded static message at the beginning  
+    useEffect(() => {
+      setMessages([
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+        {
+          _id: 2,
+          text: 'This is a system message',
+          createdAt: new Date(),
+          system: true,
+        },
+      ]);
+    }, []);
+
     return (
         <View style={[styles.container, {backgroundColor: background}]}>
             <Text>Welcome to the Chat App</Text>
+            <GiftedChat
+              messages={messages}
+              renderBubble={renderBubble}
+              onSend={messages => onSend(messages)}
+              user={{
+                _id: 1
+              }}
+            />
+             { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
         </View>
  );
 }
